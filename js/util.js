b09b4fd10642fa4aa7361824973ddc979319fc66
王广开 <416266475@qq.com>
2017-01-16 11:07:46 +0800

    各种测试

---
+++ js/util.js
@@ -0,0 +1,51 @@
+/**
+ * 说明：工具类
+ */
+var Util = {
+  checkUserInStorage : function(loginName){
+    var mpOpenUserNames = localStorage.getItem('mpOpenUserNames');
+    if(mpOpenUserNames){
+      var loginNameAy = mpOpenUserNames.split(',');
+      var loginNameAyLen = loginNameAy.length;
+      for(var i = 0; i < loginNameAyLen; i++){
+        var getLoginName = loginNameAy[i];
+        if(getLoginName == loginName){
+          return true;
+        }
+      }
+    }
+  },
+  setUnUpdateStorage : function(loginName){
+    var mpOpenUserNames = localStorage.getItem('mpOpenUserNames');
+    var loginNameAy = [];
+    if(mpOpenUserNames){
+      loginNameAy = mpOpenUserNames.split(',');
+      var loginNameAyLen = loginNameAy.length;
+      for(var i = 0; i < loginNameAyLen; i++){
+        var getLoginName = loginNameAy[i];
+        if(getLoginName == loginName){
+          return;
+        }
+      }
+    }
+    loginNameAy.push(loginName);
+    localStorage.setItem('mpOpenUserNames', loginNameAy);
+  },
+  clearUnUpdateStorage : function(loginName){
+    var mpOpenUserNames = localStorage.getItem('mpOpenUserNames');
+    if(mpOpenUserNames){
+      var loginNameAy = mpOpenUserNames.split(',');
+      var loginNameAyLen = loginNameAy.length;
+      if(loginNameAyLen){
+        for(var i = 0; i < loginNameAyLen; i++){
+          var getLoginName = loginNameAy[i];
+          if(getLoginName == loginName){
+            loginNameAy.splice(i, 1);
+            break;
+          }
+        }
+        localStorage.setItem('mpOpenUserNames', loginNameAy);
+      }
+    }
+  },
+};
