8a62dddcdcd8e898269aa0da0fdd967c8b199b9b
王广开 <41666475@qq.com>
2019-02-26 17:58:46 +0800

    增加eslint

--- js/export1.js
+++ js/export1.js
@@ -1,11 +1,10 @@
-'use strict';
 
 export function f() {
-  alert('导出的函数')
+  alert('导出的函数');
 }
 
 export function f2() {
-  alert('导出的函数2')
+  alert('导出的函数2');
 }
 export const v1 = 4;
-export default {f,v1}
+export default { f, v1 };
